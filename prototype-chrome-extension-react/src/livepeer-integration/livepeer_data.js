aeb964368a5c8075ee833eb5a2d326474b6e1cc0
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-07-31 17:24:30 -0400

    removed prototype-chrone-extension due to code split deprecation errors

--- prototype-chrome-extension-react/src/livepeer-integration/livepeer_data.js
+++
@@ -1,5 +0,0 @@
-export const livepeer_data = {
-    "livepeerServerUrl": "rtmp://rtmp.livepeer.com/live/",
-    "streamKey": "example stream key goes here",
-    "playbackUrl": "https://cdn.livepeer.com/hls/unique_playback_id_goes_here"
-}
