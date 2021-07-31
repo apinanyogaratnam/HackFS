bb810882438ec5c78fa3e9be7f0ea79728834f93
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-07-31 16:30:05 -0400

    added sample livepeer data

---
+++ prototype-chrome-extension-react/src/livepeer-integration/livepeer_data.js
@@ -0,0 +1,5 @@
+export const livepeer_data = {
+    "livepeerServerUrl": "rtmp://rtmp.livepeer.com/live/",
+    "streamKey": "example stream key goes here",
+    "playbackUrl": "https://cdn.livepeer.com/hls/unique_playback_id_goes_here"
+}
