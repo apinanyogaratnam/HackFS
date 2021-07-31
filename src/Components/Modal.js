296485a34af89c28b429a4511f7b1cda05ad7f25
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-07-31 17:30:53 -0400

    moved sub dir to main dir

---
+++ src/Components/Modal.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { X } from 'react-feather';
+import Draggable from 'react-draggable';
+import { ModalContext } from '../Contexts/ModalProvider';
+import livepeer_data from '../livepeer-integration/livepeer_data';
+
+const Modal = () => {
+  return (
+    <ModalContext.Consumer>
+      {({ windowPosition, hasDraggedWindowPosition, extensionId, getExtensionId }) => (
+        <Draggable
+          handle=".modal-handle"
+          defaultPosition={{x: windowPosition.x, y: windowPosition.y}}
+          position={hasDraggedWindowPosition ? { x: windowPosition.x, y: windowPosition.y } : null}
+        >
+          <div id="modal" className="modal-window" style={{
+            transform: windowPosition,
+        }}>
+            <div className="modal-window-inner-border">
+                <>
+                  <div className="modal-body">
+                    <div className="modal-handle">
+                      <div className="modal-close-button">
+                        <X color="#5d6484" size="14" />
+                      </div>
+                    </div>
+                    <div className="modal-content">
+                      <h3>{extensionId}</h3>
+                      <button
+                        onClick={getExtensionId}
+                        className="modal-button"
+                      >
+                        Get Extension ID
+                      </button>
+                      <h4>
+                        Connect via OBS Studio:
+                        <br />
+                        Set OBS settings: settings>stream set service to custom
+                        <br />
+                        set server to: {livepeer_data.livepeerServerUrl}
+                        <br />
+                        set stream key to: {livepeer_data.streamKey}
+                      </h4>
+                    </div>
+                  </div>
+                </>
+              </div>
+          </div>
+        </Draggable>
+      )}
+    </ModalContext.Consumer>
+  );
+};
+
+export default Modal;
---
+++ src/livepeer-integration/livepeer_data.js
@@ -0,0 +1,7 @@
+const livepeer_data = {
+    "livepeerServerUrl": "rtmp://rtmp.livepeer.com/live/",
+    "streamKey": "example stream key goes here",
+    "playbackUrl": "https://cdn.livepeer.com/hls/unique_playback_id_goes_here"
+}
+
+export default livepeer_data;
