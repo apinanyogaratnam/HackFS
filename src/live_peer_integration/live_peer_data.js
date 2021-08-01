04fe73a41cec6ac907a41defe7a0e3d50f6e65dc
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-08-01 11:42:00 -0400

    updated playback url

--- src/live_peer_integration/live_peer_data.js
+++ src/live_peer_integration/live_peer_data.js
@@ -1,7 +1,7 @@
 const live_peer_data = {
     "livePeerServerUrl": "rtmp://rtmp.livepeer.com/live/",
     "streamKey": "example stream key goes here",
-    "playbackUrl": "https://cdn.livepeer.com/hls/unique_playback_id_goes_here"
+    "playbackUrl": "https://cdn.livepeer.com/hls/unique_playback_id_goes_here/index.m3u8"
 }
 
 export default live_peer_data;
