997d12ffed5845b50c8f1d374c1170057eb84ca2
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-08-01 11:47:03 -0400

    updated api call

--- src/live_peer_integration/live_peer_api.js
+++ src/live_peer_integration/live_peer_api.js
@@ -6,40 +6,44 @@ const LivePeerAPI = () => {
     const base_url = "https://livepeer.com/api/";
     const post_url = base_url + "stream";
     const example_api_key = "31234v-asr-qwe-rxbsdr";
+    const header = {
+        headers: {
+            "content-type": "application/json",
+            authorization: "Bearer " + api_key
+        }
+    }
+    const profiles = {
+        name: "example_stream",
+        "profiles": [
+            {
+                "name": "720p",
+                "bitrate": 2000000,
+                "fps": 30,
+                "width": 1280,
+                "height": 720
+            },
+            {
+                "name": "480p",
+                "bitrate": 1000000,
+                "fps": 30,
+                "width": 854,
+                "height": 480
+            },
+            {
+                "name": "360p",
+                "bitrate": 500000,
+                "fps": 30,
+                "width": 640,
+                "height": 360
+            }
+        ]
+    }
 
     const fetchPostData = () => {
-        axios.post(post_url, {
-            name: "example_stream",
-            "profiles": [
-                {
-                  "name": "720p",
-                  "bitrate": 2000000,
-                  "fps": 30,
-                  "width": 1280,
-                  "height": 720
-                },
-                {
-                  "name": "480p",
-                  "bitrate": 1000000,
-                  "fps": 30,
-                  "width": 854,
-                  "height": 480
-                },
-                {
-                  "name": "360p",
-                  "bitrate": 500000,
-                  "fps": 30,
-                  "width": 640,
-                  "height": 360
-                }
-            ]
-        },
-        {
-            headers: {
-                "content-type": "application/json",
-                authorization: "Bearer " + api_key
-            }
-        }).then(res => {
+        axios.post(post_url,
+        header,
+        profiles
+        ).then(res => {
             setResponse(res);
         }).catch(err => {
             console.log(err);
