9f67f49d70bca44c5a731d2d67f3e0856a4004a8
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-08-01 11:41:31 -0400

    completed create stream api call without testing

--- src/live_peer_integration/live_peer_api.js
+++ src/live_peer_integration/live_peer_api.js
@@ -1,8 +1,54 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState, useEffect } from 'react'
 
 const LivePeerAPI = () => {
+    const [response, setResponse] = useState([]);
     const base_url = "https://livepeer.com/api/";
-    const post_url = "https://livepeer.com/api/stream";
+    const post_url = base_url + "stream";
+    const example_api_key = "31234v-asr-qwe-rxbsdr";
+
+    const fetchPostData = () => {
+        axios.post(post_url, {
+            name: "example_stream",
+            "profiles": [
+                {
+                  "name": "720p",
+                  "bitrate": 2000000,
+                  "fps": 30,
+                  "width": 1280,
+                  "height": 720
+                },
+                {
+                  "name": "480p",
+                  "bitrate": 1000000,
+                  "fps": 30,
+                  "width": 854,
+                  "height": 480
+                },
+                {
+                  "name": "360p",
+                  "bitrate": 500000,
+                  "fps": 30,
+                  "width": 640,
+                  "height": 360
+                }
+            ]
+        },
+        {
+            headers: {
+                "content-type": "application/json",
+                authorization: "Bearer " + api_key
+            }
+        }).then(res => {
+            setResponse(res);
+        }).catch(err => {
+            console.log(err);
+        })
+    }
+
+    useEffect(() => {
+        fetchPostData();
+    }, [])
 
     const exampleResponse = {
         "name":"test_stream",
