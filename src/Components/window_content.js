b6864e4e8d6b9b77c641603534f4e62854cfc6a3
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-08-02 18:52:04 -0400

    removed unnecessary text on window

--- src/Components/window_content.js
+++ src/Components/window_content.js
@@ -10,14 +10,13 @@ const WindowContent = () => {
     return (
         <div>
             <h4 className="text">
-                <LivePeerAPI />
                 Connect via OBS Studio:
                 <br />
                 Set OBS settings: settings {'>'} stream. set service to custom
                 <br />
                 set server to: {live_peer_data.livePeerServerUrl}
                 <br />
-                set stream key to: {live_peer_data.streamKey}
+                <LivePeerAPI />
             </h4>
             <ShakaPlayer autoPlay src="https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd" />
         </div>
