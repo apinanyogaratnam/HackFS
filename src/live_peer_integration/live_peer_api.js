dc2750bd2e1547feb40e6b8b1cd56811d7955f74
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-08-02 12:24:33 -0400

    installed node package and called api without logging object as of now

--- src/live_peer_integration/live_peer_api.js
+++ src/live_peer_integration/live_peer_api.js
@@ -2,69 +2,40 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react'
 
 const LivePeerAPI = () => {
-    const [response, setResponse] = useState([]);
-    const base_url = "https://livepeer.com/api/";
-    const post_url = base_url + "stream";
-    const example_api_key = "";
-    const header = {
-        headers: {
-            "content-type": "application/json",
-            authorization: `Bearer ${example_api_key}`
-        }
-    }
-    const profiles = {
-        name: "example_stream",
-        profiles: [
-            {
-                "name": "720p",
-                "bitrate": 2000000,
-                "fps": 30,
-                "width": 1280,
-                "height": 720
-            },
-            {
-                "name": "480p",
-                "bitrate": 1000000,
-                "fps": 30,
-                "width": 854,
-                "height": 480
-            },
-            {
-                "name": "360p",
-                "bitrate": 500000,
-                "fps": 30,
-                "width": 640,
-                "height": 360
-            }
-        ]
+    const Livepeer = require('livepeer-nodejs');
+    const api_key = process.env.REACT_APP_API_KEY;
+    const livepeerObject = new Livepeer(api_key);
+
+    const stream = async () => {
+        await livepeerObject.Stream.create({
+            "name": "test_stream", 
+            "profiles": [
+                {
+                    "name": "720p",
+                    "bitrate": 2000000,
+                    "fps": 30,
+                    "width": 1280,
+                    "height": 720
+                },
+                {
+                    "name": "480p",
+                    "bitrate": 1000000,
+                    "fps": 30,
+                    "width": 854,
+                    "height": 480
+                },
+                {
+                    "name": "36p",
+                    "bitrate": 500000,
+                    "fps": 30,
+                    "width": 640,
+                    "height": 360
+                },
+            ]
+        });
     }
 
-    const fetchPostData = () => {
-        axios.post(post_url,
-        header,
-        profiles
-        ).then(res => {
-            console.log(res);
-            setResponse(res);
-        })
-    }
-
-    useEffect(() => {
-        fetchPostData();
-    }, [])
-
-    const exampleResponse = {
-        "name":"test_stream",
-        "profiles":[
-            {"name":"720p","bitrate":2000000,"fps":30,"width":1280,"height":720},
-            {"name":"480p","bitrate":1000000,"fps":30,"width":854,"height":480},
-            {"name":"360p","bitrate":500000,"fps":30,"width":640,"height":360}
-        ],
-        "id":"ijkl61f3-95bd-4971-a7b1-4dcb5d39e78a",
-        "createdAt":1596081229373,
-        "streamKey":"abcd-uimq-jtgy-x98v",
-        "playbackId":"efghb2mxupongp5k"
-    }
+    console.log(stream.name);
 
     return (
         <div>
