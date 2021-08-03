c5fa6db5109e64620b26af0aeb1c4be1f83fbd0c
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-08-03 19:51:33 -0400

    added necessary imports for player

--- src/Components/window_content.js
+++ src/Components/window_content.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import live_peer_data from '../live_peer_integration/live_peer_data';
 import '../css/window.scss';
 import ShakaPlayer from 'shaka-player-react';
+import "node_modules/video-react/dist/video-react.css";
 import 'shaka-player/dist/controls.css';
 import LivePeerAPI from '../live_peer_integration/live_peer_api';
+import { Player } from 'video-react';
 
 const WindowContent = () => {
     const example_video_url = "https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd";
