2d289ce678d254af67d6e8fc305661efd1110d5e
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-08-03 19:59:47 -0400

    created main recorder file for recording process

---
+++ src/Components/main_recorder.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const MainRecorder = () => {
+
+    // const startRecording = async () => {
+
+    // }
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default MainRecorder;
