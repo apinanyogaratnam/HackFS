015a0914c73dbc4ed0881f377f0be75ee85cc2ea
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-08-03 20:14:28 -0400

    updated video player implemented

--- src/Components/video_player.js
+++ src/Components/video_player.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Player } from 'video-react';
-import "node_modules/video-react/dist/video-react.css"; // import css
+import "video-react/dist/video-react.css"; // import css
 
 const VideoPlayer = () => {
     return (
--- src/Components/window_content.js
+++ src/Components/window_content.js
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import live_peer_data from '../live_peer_integration/live_peer_data';
 import '../css/window.scss';
 import ShakaPlayer from 'shaka-player-react';
-import "node_modules/video-react/dist/video-react.css";
+// import "../../node_modules/video-react/dist/video-react.css";
 import 'shaka-player/dist/controls.css';
 import LivePeerAPI from '../live_peer_integration/live_peer_api';
 import { Player } from 'video-react';
+import VideoPlayer from './video_player.js';
 
 const WindowContent = () => {
     const example_video_url = "https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd";
@@ -21,7 +22,8 @@ const WindowContent = () => {
                 <br />
                 <LivePeerAPI className="live-peer-data"/>
             </h4>
-            <ShakaPlayer autoPlay src="" />
+            {/* <ShakaPlayer autoPlay src="" /> */}
+            <VideoPlayer />
         </div>
     );
 }
