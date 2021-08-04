ffae26db151f2fe82ddee3befe56c9006ba22145
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-08-03 23:01:42 -0400

    imported recordrtc without installing node package

--- src/Components/main_recorder.js
+++ src/Components/main_recorder.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import RecordRTC, { RecordRTCPromisesHandler } from 'recordrtc';
 
 const MainRecorder = () => {
     const [recorder, setRecorder] = useState(null);
