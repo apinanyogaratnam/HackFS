aa05f0981cc44c32bdc058ab4bc237c5ddc5de76
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-08-03 22:35:27 -0400

    completed anon start recording function

--- src/Components/main_recorder.js
+++ src/Components/main_recorder.js
@@ -4,13 +4,18 @@ const MainRecorder = () => {
     const [recorder, setRecorder] = useState(null);
     const [stream, setStream] = useState(null);
     const [videoBlob, setVideoUrlBlob] = useState(null);
-    
+
     const startRecording = async () => {
-        let stream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
-        let recorder = new RecordRTCPromisesHandler(stream, {
+        let mediaDevices = navigator.mediaDevices;
+        const stream = await mediaDevices.getUserMedia({video: true, audio: true});
+        const recorder = new RecordRTCPromisesHandler(stream, {
             type: 'video'
         });
-        recorder.startRecording();
+        
+        await recorder.startRecording();
+        setRecorder(recorder);
+        setStream(stream);
+        setVideoUrlBlob(null);
     }
 
     return (
