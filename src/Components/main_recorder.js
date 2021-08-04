ba8c1807b2db7cd2123d70317b9650083818e466
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-08-03 22:31:29 -0400

    state declarations complete

--- src/Components/main_recorder.js
+++ src/Components/main_recorder.js
@@ -1,15 +1,22 @@
 import React from 'react'
 
 const MainRecorder = () => {
-
-    // const startRecording = async () => {
-
-    // }
+    const [recorder, setRecorder] = useState(null);
+    const [stream, setStream] = useState(null);
+    const [videoBlob, setVideoUrlBlob] = useState(null);
+    
+    const startRecording = async () => {
+        let stream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
+        let recorder = new RecordRTCPromisesHandler(stream, {
+            type: 'video'
+        });
+        recorder.startRecording();
+    }
 
     return (
         <div>
             <button>Start streaming</button>
-            
+
         </div>
     )
 }
