538dcd20e4278a8149dc7770f030271a1bccf8c0
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-08-04 10:53:23 -0400

    fixed typo error

--- src/Components/main_recorder.js
+++ src/Components/main_recorder.js
@@ -40,7 +40,7 @@ const MainRecorder = () => {
     return (
         <div>
             <button onClick={startRecording()}>Start streaming</button>
-            <button onClick={stopStreaming()}>Stop streaming</button>
+            <button onClick={stopRecording()}>Stop streaming</button>
         </div>
     )
 }
