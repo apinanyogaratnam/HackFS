8b48db48676cfc59f26586cfd26100ebff787202
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-08-03 20:04:29 -0400

    added player react component without importing it into main window

--- src/Components/video_player.js
+++ src/Components/video_player.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Player } from 'video-react';
+import "node_modules/video-react/dist/video-react.css"; // import css
 
 const VideoPlayer = () => {
     return (
         <div>
-            
+            <Player
+                playsInline
+                poster="/assets/poster.png"
+                src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+            />
         </div>
-    )
+    );
 }
 
 export default VideoPlayer;
