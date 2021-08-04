21f0840f184f69f9557651672ba623349c597b77
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-08-04 13:38:53 -0400

    made sitel trailer url a const var

--- src/Components/video_player.js
+++ src/Components/video_player.js
@@ -3,13 +3,16 @@ import { Player } from 'video-react';
 import "video-react/dist/video-react.css"; // import css
 
 const VideoPlayer = () => {
+    const sintel_trailer_url = "https://media.w3.org/2010/05/sintel/trailer_hd.mp4";
+    const test_url = "https://cdn.livepeer.com/hls/105dqxa9zszpduw5/index.m3u8";
+
     return (
         <div>
             {/* React component for video player */}
             <Player
                 playsInline
                 poster="/assets/poster.png"
-                src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+                src={sintel_trailer_url}
             />
         </div>
     );
