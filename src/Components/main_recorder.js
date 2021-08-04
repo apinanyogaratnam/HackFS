ff9d0041b887036d956da1d37f543e9ce24f64ae
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-08-04 10:14:15 -0400

    added comments to main recorder file

--- src/Components/main_recorder.js
+++ src/Components/main_recorder.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 import RecordRTC, { RecordRTCPromisesHandler } from 'recordrtc';
 
 const MainRecorder = () => {
+    // using states to store data relatively depending on user's actions
     const [recorder, setRecorder] = useState(null);
     const [stream, setStream] = useState(null);
     const [videoBlob, setVideoUrlBlob] = useState(null);
 
+    // function to start recording video and audio
     const startRecording = async () => {
         let mediaDevices = navigator.mediaDevices;
         const stream = await mediaDevices.getUserMedia({video: true, audio: true});
@@ -14,16 +16,21 @@ const MainRecorder = () => {
         });
         
         await recorder.startRecording();
+
+        // storing the data in the states
         setRecorder(recorder);
         setStream(stream);
         setVideoUrlBlob(null);
     }
 
+    // function to stop recording audio and video
     const stopRecording = async () => {
         if (recorder) {
             await recorder.stopRecording();
             const blob = await recorder.getBlob();
             stream.stop()
+
+            // storing the data in the states
             setVideoUrlBlob(blob);
             setStream(null);
             setRecorder(null);
