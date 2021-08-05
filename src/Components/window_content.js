b64c1d0f8d5e53a182fb1ea9724c5ab1bca436d1
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-08-04 20:39:30 -0400

    current progess with css

--- src/Components/window_content.js
+++ src/Components/window_content.js
@@ -7,14 +7,64 @@ import LivePeerAPI from '../live_peer_integration/live_peer_api';
 import { Player } from 'video-react';
 import VideoPlayer from './video_player.js';
 import MainRecorder from './main_recorder';
+import { RecordRTCPromisesHandler } from 'recordrtc';
+import { saveAs } from 'file-saver';
 
 const WindowContent = () => {
     // url to play in the video player
     const example_video_url = "https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd";
 
+    // using states to store data relatively depending on user's actions
+    const [recorder, setRecorder] = useState(null);
+    const [stream, setStream] = useState(null);
+    const [videoBlob, setVideoUrlBlob] = useState(null);
+
+    // function to start recording video and audio
+    const startRecording = async () => {
+        let mediaDevices = navigator.mediaDevices;
+        const stream = await mediaDevices.getDisplayMedia({video: true, audio: false});
+        const recorder = new RecordRTCPromisesHandler(stream, {
+            type: 'video'
+        });
+        
+        await recorder.startRecording();
+
+        // storing the data in the states
+        setRecorder(recorder);
+        setStream(stream);
+        setVideoUrlBlob(null);
+    }
+
+    // function to stop recording audio and video
+    const stopRecording = async () => {
+        if (recorder) {
+            await recorder.stopRecording();
+            const blob = await recorder.getBlob();
+            stream.stop()
+
+            // storing the data in the states
+            setVideoUrlBlob(blob);
+            setStream(null);
+            setRecorder(null);
+            
+            return;
+        }
+
+        alert("No video streaming currently to stop");
+    }
+
+    const downloadRecording = () => {
+        if (videoBlob) {
+            saveAs(videoBlob, "recording.webm");
+            return;
+        }
+
+        alert("No video streamed to download");
+    }
+
     return (
         <div>
-            <h4 className="text">
+            {/* <h4 className="text">
                 Connect via OBS Studio:
                 <br />
                 Set OBS settings: settings {'>'} stream. set service to custom
@@ -22,10 +72,19 @@ const WindowContent = () => {
                 set server to: {live_peer_data.livePeerServerUrl}
                 <br />
                 <LivePeerAPI className="live-peer-data"/>
-            </h4>
-            <MainRecorder />
+            </h4> */}
+            <div className="top-container">
+
+            </div>
+            <div className="middle-container">
+
+            </div>
+            <div className="bottom-container">
+                
+            </div>
+            {/* <MainRecorder /> */}
             {/* <ShakaPlayer autoPlay src="" /> */}
-            <VideoPlayer />
+            {/* <VideoPlayer /> */}
         </div>
     );
 }
