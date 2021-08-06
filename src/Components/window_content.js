6a58a4dd4419f2c4cb74f4f36e2008043f4a403f
Jonathan Pynes <jonpynes@gmail.com>
2021-08-06 17:33:59 -0500

    Added CSS to top of Window_content

--- src/Components/window_content.js
+++ src/Components/window_content.js
@@ -20,89 +20,101 @@ import styled from "styled-components";
 import content from "../Livepeer/content_livepeer";
 
 const WindowContent = () => {
-  // livepeer api package
-  const Livepeer = require("livepeer-nodejs");
-  const apiKey = process.env.REACT_APP_API_KEY;
-  const livepeerObject = new Livepeer(apiKey);
+    // livepeer api package
+    const Livepeer = require("livepeer-nodejs");
+    const apiKey = process.env.REACT_APP_API_KEY;
+    const livepeerObject = new Livepeer(apiKey);
 
-  // url to play in the video player
-  const example_video_url =
-    "https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd";
-  var live_peer_demo_url =
-    "https://mdw-cdn.livepeer.com/recordings/2bf2222d-b82e-4758-bce8-2a6fa04e5550/index.m3u8";
-  live_peer_demo_url =
-    "https://mdw-cdn.livepeer.com/recordings/bbc3ca03-e570-4c55-b97d-afc2aa41fdc8/source.mp4";
+    // url to play in the video player
+    const example_video_url =
+        "https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd";
+    var live_peer_demo_url =
+        "https://mdw-cdn.livepeer.com/recordings/2bf2222d-b82e-4758-bce8-2a6fa04e5550/index.m3u8";
+    live_peer_demo_url =
+        "https://mdw-cdn.livepeer.com/recordings/bbc3ca03-e570-4c55-b97d-afc2aa41fdc8/source.mp4";
 
-  // using state to store data relatively (livepeer response data)
-  const [data, setData] = useState([]);
-  const [streamUrl, setStreamUrl] = useState(null);
+    // using state to store data relatively (livepeer response data)
+    const [data, setData] = useState([]);
+    const [streamUrl, setStreamUrl] = useState(null);
 
-  // create a stream using livepeer's api
-  const startStream = () => {
-    livepeerObject.Stream.create(content).then((res) => {
-      console.log(res);
-      setData(res);
-    });
-  };
+    // create a stream using livepeer's api
+    const startStream = () => {
+        livepeerObject.Stream.create(content).then((res) => {
+            console.log(res);
+            setData(res);
+        });
+    };
 
-  useEffect(() => {
-    startStream();
-  }, []);
+    useEffect(() => {
+        startStream();
+    }, []);
 
-  const stopStream = async () => {
-    const url = `https://livepeer.com/api/stream/${data.id}/terminate`;
+    const stopStream = async () => {
+        const url = `https://livepeer.com/api/stream/${data.id}/terminate`;
 
-    await axios.delete(url, {
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-      },
-    });
-  };
+        await axios.delete(url, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`,
+            },
+        });
+    };
 
-  const someData = async () => {
-    const url = `https://livepeer.com/api/session?limit=20&parentId=${data.id}`;
+    const someData = async () => {
+        const url = `https://livepeer.com/api/session?limit=20&parentId=${data.id}`;
 
-    const listOfAllStreams = await axios.get(url, {
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-      },
-    });
-    console.log(listOfAllStreams);
-    console.log(listOfAllStreams.data[0].mp4Url);
-    setStreamUrl(listOfAllStreams.data[0].mp4Url);
-  };
+        const listOfAllStreams = await axios.get(url, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`,
+            },
+        });
+        console.log(listOfAllStreams);
+        console.log(listOfAllStreams.data[0].mp4Url);
+        setStreamUrl(listOfAllStreams.data[0].mp4Url);
+    };
 
-  // console.log(window.location.href);
+    // console.log(window.location.href);
 
-  return (
-    <Window_Content_WrapperCSS>
-      <h4 className="text">
-        Connect via OBS Studio:
-        <br />
-        Set OBS settings: settings {">"} stream. set service to custom
-        <br />
-        set server to: {live_peer_data.livePeerServerUrl}
-        <br />
-        {/* <LivePeerAPI className="live-peer-data"/> */}
-        <p>Stream Key: {data.streamKey}</p>
-        <p>
-          Playback URL: https://cdn.livepeer.com/hls/{data.playbackId}
-          /index.m3u8
-        </p>
-        <p>Stream id: {data.id}</p>
-        <p>Link URL: {streamUrl}</p>
-      </h4>
-      <button onClick={someData}>Stop Streaming</button>
-      {/* <VideoPlayer playsInLine src="https://mdw-cdn.livepeer.com/recordings/9ffba687-6059-4aa3-8d12-0235a79701aa/source.mp4" /> */}
-      <ShakaPlayer src={streamUrl} />
-    </Window_Content_WrapperCSS>
-  );
+    return (
+        <Window_Content_WrapperCSS>
+            <h4 className="text">
+                <URLWrapperCSS>
+                    <p>URL:</p> <URLBoxCSS>{streamUrl}</URLBoxCSS>
+                </URLWrapperCSS>
+                Connect via OBS Studio:
+                <br />
+                Set OBS settings: settings {">"} stream. set service to custom
+                <br />
+                set server to: {live_peer_data.livePeerServerUrl}
+                <br />
+                {/* <LivePeerAPI className="live-peer-data"/> */}
+                <p>Stream Key: {data.streamKey}</p>
+                <p>
+                    Playback URL: https://cdn.livepeer.com/hls/{data.playbackId}
+                    /index.m3u8
+                </p>
+                <p>Stream id: {data.id}</p>
+            </h4>
+            <button onClick={someData}>Stop Streaming</button>
+            {/* <VideoPlayer playsInLine src="https://mdw-cdn.livepeer.com/recordings/9ffba687-6059-4aa3-8d12-0235a79701aa/source.mp4" /> */}
+            <ShakaPlayer src={streamUrl} />
+        </Window_Content_WrapperCSS>
+    );
 };
 
 const Window_Content_WrapperCSS = styled.div`
-  background-color: purple;
-  width: 100%;
-  height: 100%;
+    background-color: purple;
+    width: 100%;
+    height: 100%;
 `;
-
+const URLWrapperCSS = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+const URLBoxCSS = styled.div`
+    width: 70%;
+    height: 2rem;
+    border-style: solid;
+    border-radius: 8px;
+    align-self: center;
+`;
 export default WindowContent;
