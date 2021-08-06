1c79bcc39f36f44f1354be871d51762a7e051d62
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-08-06 15:14:13 -0400

    formatted modal js file code

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -6,36 +6,37 @@ import '../css/window.scss';
 import WindowContent from './window_content';
 
 const Modal = () => {
+
   return (
-    <ModalContext.Consumer>
-      {({ windowPosition, hasDraggedWindowPosition, extensionId, getExtensionId }) => (
-        <Draggable
-          handle=".modal-handle"
-          defaultPosition={{x: windowPosition.x, y: windowPosition.y}}
-          position={hasDraggedWindowPosition ? { x: windowPosition.x, y: windowPosition.y } : null}
-        >
-          <div id="modal" className="modal-window" style={{
-            transform: windowPosition,
-          }}>
-              <div className="modal-window-inner-border">
-                  <>
-                      <div className="modal-body">
-                        <div className="modal-handle">
-                            <div className="modal-close-button">
+      <ModalContext.Consumer>
+        {({ windowPosition, hasDraggedWindowPosition, extensionId, getExtensionId }) => (
+            <Draggable
+              handle=".modal-handle"
+              defaultPosition={{x: windowPosition.x, y: windowPosition.y}}
+              position={hasDraggedWindowPosition ? { x: windowPosition.x, y: windowPosition.y } : null}
+            >
+              <div id="modal" className="modal-window" style={{
+                transform: windowPosition,
+              }}>
+                  <div className="modal-window-inner-border">
+                      <>
+                          <div className="modal-body">
+                            <div className="modal-handle">
+                                <div className="modal-close-button">
+                                </div>
+                                <h6 className="handle-text">Click to drag</h6>
                             </div>
-                            <h6 className="handle-text">Click to drag</h6>
-                        </div>
-                        <div className="modal-content">
-                          <h3>{extensionId}</h3>
-                          <WindowContent />
-                        </div>
-                      </div>
-                  </>
+                            <div className="modal-content">
+                              <h3>{extensionId}</h3>
+                              <WindowContent />
+                            </div>
+                          </div>
+                      </>
+                  </div>
               </div>
-          </div>
-        </Draggable>
-      )}
-    </ModalContext.Consumer>
+            </Draggable>
+        )}
+      </ModalContext.Consumer>
   );
 };
 
